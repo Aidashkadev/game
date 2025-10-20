c171070e97720e7c5529d4cd5aecdf506d8fa406
kolgeldieva_a@iuca.kg <kolgeldieva_a@iuca.kg>
2025-10-20 22:57:29 +0600

    добавили html

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Cat
 
-# Create your views here.
+def index(request):
+    cats = Cat.objects.all()
+    return render(request, 'game/index.html', {'cats': cats})
